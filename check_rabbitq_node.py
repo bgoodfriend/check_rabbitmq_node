5eefbb8d9a0df7e295640df07453260ffb91e00e
Bob Goodfriend <bgoodfriend@localhost.localdomain>
2020-08-11 20:46:07 -0500

    initial

---
+++ check_rabbitq_node.py
@@ -0,0 +1,87 @@
+#!/bin/python
+
+import requests
+from requests.exceptions import HTTPError
+import argparse
+import sys
+
+
+parser = argparse.ArgumentParser(description='Check RabbitMQ Node')
+parser.add_argument('-hostname', help='RabbitMQ host name',
+                    required=True)
+parser.add_argument('-port', help='RabbitMQ port',
+                    default='15672')
+parser.add_argument('-node', help='RabbitMQ node',
+                    default = "")
+parser.add_argument('-user', help='RabbitMQ username',
+                    default='guest')
+parser.add_argument('-password', help='RabbitMQ password',
+                    default='guest')
+parser.add_argument('-ssl', help='Use SSL', action='store_true')
+parser.add_argument('-rabbitname', help='Rabbit name (default: rabbit)',
+                    default='rabbit')
+
+args = parser.parse_args()
+
+# This script's output follows the Nagios/Icinga plugin standard:
+# https://nagios-plugins.org/doc/guidelines.html
+
+# This script consumes typical http://<rabbitmqhost>/api/nodes
+# For a complete RabbitMQ solution, additionally test at least 
+# http://<rabbitmqhost>/api/aliveness-test/
+
+# Another approach to monitoring RabbitMQ in containers, is to let the 
+# containers export their own metrics to Prometheus.  Examples:
+# https://github.com/kbudde/rabbitmq_exporter
+# https://github.com/deadtrickster/prometheus_rabbitmq_exporter
+
+# Example function test procedure:
+# 1. local deploy community docker image, create named node:
+# % sudo docker run -it --rm --hostname my-rabbit --name my-rabbit -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+# 2. Verify it's there:
+# # All nodes (array of 1 node):
+# curl -u guest:guest -X GET http://localhost:15672/api/nodes | jq
+# # Just our named node:
+# curl -u guest:guest -X GET http://localhost:15672/api/nodes/rabbit@my-rabbit/ | jq
+# 3. Default check all nodes:
+# % ./check_rabbitmq_node -hostname localhost
+
+ERROR = {'OK': 0, 'WARNING': 1, 'CRITICAL': 2, 'UNKNOWN': 3}
+warn_msg = []
+crit_msg = []
+ok_msg = []
+
+protocol = "s" if args.ssl else ""
+url = "http%s://%s:%s/api/nodes/%s" % ( protocol, args.hostname, args.port, args.node )
+print(url)
+
+try:
+    response = requests.get(url, auth=(args.user, args.password))
+    
+    
+    # If the response was successful, no Exception will be raised
+    response.raise_for_status()
+except HTTPError as http_err:
+    print("UNKNOWN:HTTP error occurred: ", http_err )
+    print(sys.exit(3))
+except Exception as err:
+    print("UNKNOWN:Other error occured: ", err )
+    print(sys.exit(3))
+else:
+    print('Success!')
+
+try:
+    nodes = response.json()
+except ValueError:
+    print("UNKNOWN:No JSON in Response from ", url)
+
+# /api/nodes returns a list of dicts...
+# Single node endpoint returns dict.
+if isinstance(nodes, dict):
+    nodes = [ nodes ]
+
+for node in nodes:
+    print(node['name'])
+    print(node['mem_used'])
+    print(node['mem_limit'])
+
